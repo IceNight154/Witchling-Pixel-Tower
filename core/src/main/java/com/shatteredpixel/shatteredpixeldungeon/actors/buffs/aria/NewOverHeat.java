937fc470443b7f965a54c1ad509d71546c14a2f6
Mocha <mtj2010@naver.com>
2025-09-07 07:56:12 +0900

    오버히트 코드에 주석 추가

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/buffs/aria/NewOverHeat.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/buffs/aria/NewOverHeat.java
@@ -20,29 +20,31 @@ import com.watabou.utils.PointF;
 import com.watabou.utils.Random;
 
 public class NewOverHeat extends Buff implements ActionIndicator.Action {
-    {
-        type = buffType.NEUTRAL;
 
-        revivePersists = true;
+    {
+        type = buffType.NEUTRAL; // 버프 타입은 중립으로, NEGATIVE로 할 경우 청정의 물약 등에 제거될 수 있으니 이런 버프의 경우 중립이 낫습니다.
+
+        revivePersists = true; // 죽어도 남아있게 하는 코드입니다.
     }
 
     public enum ElementType {
-        FIRE("fire"),
-        WATER("water"),
-        EARTH("earth"),
-        WIND("wind");
+        FIRE("fire"), // 불 원소
+        WATER("water"), // 물 원소
+        EARTH("earth"), // 땅 원소
+        WIND("wind"); // 바람 원소
 
-        final String name;
+        final String name; // 각 원소의 이름. 일종의 key로 작용합니다.
 
-        ElementType(String name) {
+        ElementType(String name) { // 위의 name변수를 생성자에서 받아서 지정해 주는 것입니다. 건드릴 필요 없어요.
             this.name = name;
         }
 
+        // 각 원소 별 이름 문장을 간단하게 반환해 주는 메서드입니다. ElementType.FIRE.getName()을 호출하면 actors_ko.properties에서 [.element_fire=]에 지정해 준 문장에 따라 "불"이 나와요.
         public String getName() {
             return Messages.get(NewOverHeat.class, "element_"+name);
         }
 
-        public static ElementType getElementByIndex(int index) {
+        public static ElementType getElementByIndex(int index) { // int 숫자로 ElementType을 반환해 주는 메서드입니다. 0번째는 불, 1번째는 물, 2번째는 땅, 3번째는 바람이에요.
             switch (index) {
                 case 0: default:
                     return FIRE;
@@ -56,13 +58,13 @@ public class NewOverHeat extends Buff implements ActionIndicator.Action {
         }
     }
 
-    private static final int OVERHEAT_MAX = 100;
-    private static final String TXT_STATUS = "%d/%d";
+    private static final int OVERHEAT_MAX = 100; // 오버히트 게이지 최대치
+    private static final String TXT_STATUS = "%d/%d"; // 오버히트 게이지 표기 방식. 액션 인디케이터(버튼)에서 사용됩니다.
 
-    private int gauge = 0;
-    private ElementType element = ElementType.FIRE;
+    private int gauge = 0; // 현재 오버히트 게이지입니다. private이기 때문에 외부에서 접근할 수 없어요. 필요 시 cool()이나 heat()메서드로 조절해 주시면 됩니다.
+    private ElementType element = ElementType.FIRE; // 버프 제공 당시 원소는 불로 지정했습니다.
 
-    public ElementType randomElement() {
+    public ElementType randomElement() { // 현재 원소를 랜덤한 원소로 바꾸되, 현재 원소와 달라질 때까지 반복하기 때문에 항상 현재 원소와 결과 원소는 같지 않습니다.
         ElementType newElement;
         do {
             switch (Random.Int(4)) {
@@ -82,31 +84,44 @@ public class NewOverHeat extends Buff implements ActionIndicator.Action {
         } while (newElement == element);
 
         element = newElement;
-        return element;
+        return element; // 바뀐 원소를 반환합니다. 예를 들어 Glog.i()를 사용해 원소를 %s로 바꾸었다. 등의 문구를 출력할 수 있겠네요.
     }
 
-    public ElementType setElement(ElementType element) {
+    public ElementType setElement(ElementType element) { // 원소를 매개변수에 들어온 값으로 변경하고 그 결과를 반환합니다.
         this.element = element;
         return element;
     }
 
-    public ElementType getElement() {
+    public ElementType getElement() { // 현재 원소를 반환합니다.
         return element;
     }
 
+    // 원소를 매개변수에 들어온 값으로 변경하고 그 결과를 반환합니다.
+    // 다만 이 메서드는 어느 클래스에서든 사용할 수 있습니다. Dungeon.hero.buff(NewOverHeat.class)등을 통해 따로 버프 인스턴스를 찾을 필요가 없어요.
     public static ElementType changeElement(Char target, ElementType element) {
         return Buff.affect(target, NewOverHeat.class).setElement(element);
     }
 
-    public void heat(int amount) {
-        gauge = Math.min(OVERHEAT_MAX, gauge + amount);
+    public static void onChangeElement() { //원소 변경 시 작동하는 코드입니다.
+        //TODO: 원소 변경 시 나타나야 하는 이펙트, 사운드 출력 등을 추가해 주세요.
     }
 
-    public void cool(int amount) {
+    public void heat(int amount) { // 게이지를 지정한 양만큼 늘리는 메서드입니다. 최대치 이상으로 늘어나지 않습니다.
+        gauge = Math.min(OVERHEAT_MAX, gauge + amount);
+        //TODO: 필요한 경우 멜트다운 시 추가 동작을 구현해 주세요. 예시 코드는 다음과 같습니다.
+        /*
+        if (isMeltDown()) {
+            //각종 이펙트 및 데미지 입히기, 버프 주기 등. target은 NewOverHeat 버프를 가진 Char 인스턴스를 의미합니다.
+            Buff.affect(target, Exhaustion.class, 1f);
+        }
+        */
+    }
+
+    public void cool(int amount) { // 게이지를 지정한 양만큼 줄이는 메서드입니다. 최소치 이하로 감소하지 않습니다.
         gauge = Math.max(0, gauge - amount);
     }
 
-    public int heatLevel() {
+    public int heatLevel() { // 현재 게이지에 따른 오버히트 레벨을 반환합니다.
         if (gauge == 0) { //0
             return 0;
         } else if (gauge < 25) { //1~24
@@ -122,32 +137,32 @@ public class NewOverHeat extends Buff implements ActionIndicator.Action {
         }
     }
 
-    public boolean isMeltDown() {
+    public boolean isMeltDown() { // 현재 버프가 멜트다운인지 여부를 반환합니다.
         return heatLevel() == 5;
     }
 
     @Override
-    public boolean attachTo(Char target) { //버프가 붙을 때 호출
-        ActionIndicator.setAction(this);
+    public boolean attachTo(Char target) { // 버프가 붙을 때 호출
+        ActionIndicator.setAction(this); // 원소 변경 버튼을 생성합니다.
         return super.attachTo(target);
     }
 
     @Override
-    public void detach() { //버프가 제거될 때 호출, 그러나 여기에서 딱히 쓰일 일은 없음
-        ActionIndicator.clearAction();
+    public void detach() { // 버프가 제거될 때 호출, 그러나 여기에서 딱히 쓰일 일은 없음
+        ActionIndicator.clearAction(); // 원소 변경 버튼을 제거합니다.
         super.detach();
     }
 
     @Override
     public boolean act() {
-        spend(TICK);
-        gauge = Math.max(0, gauge - 1);
+        spend(TICK); //이게 없으면 무한로딩이 생기니 주의하세요.
+        gauge = Math.max(0, gauge - 1); //턴마다 1의 게이지를 감소시킵니다.
         return true;
     }
 
     @Override
-    public int icon() {
-        switch (heatLevel()) {
+    public int icon() { // 버프의 아이콘을 지정합니다.
+        switch (heatLevel()) { // 오버히트 레벨에 따라 다른 아이콘을 지정합니다.
             case 0: default:
                 return BuffIndicator.OVERHEAT_0;
             case 1:
@@ -164,34 +179,36 @@ public class NewOverHeat extends Buff implements ActionIndicator.Action {
     }
 
     @Override
-    public void tintIcon(Image icon) { //버프 아이콘 색상을 물들이는 메서드. 딱히 쓸 일은 없으니 변경하지 않음
+    public void tintIcon(Image icon) { //버프 아이콘 색상을 물들이는 메서드. 이미 아이콘 색상이 결정되어 있기 때문에 딱히 쓸 일은 없으니 변경하지 않습니다.
         super.tintIcon(icon);
     }
 
     @Override
-    public String name() {
-        if (isMeltDown()) {
+    public String name() { // 버프의 이름입니다.
+        if (isMeltDown()) { // 멜트다운일 경우 다른 이름을 적용합니다.
             return Messages.get(this, "name_meltDown", element.getName());
-        } else {
+        } else { // 멜트다운이 아닐 경우 레벨에 따른 공통 이름을 적용합니다.
             return Messages.get(this, "name", heatLevel(), element.getName());
         }
     }
 
     @Override
-    public String desc() {
+    public String desc() { // 버프의 설명문입니다.
         return Messages.get(this, "desc", heatLevel(), element.getName());
     }
 
     @Override
-    public String iconTextDisplay() {
+    public String iconTextDisplay() { // 버프에 현재 게이지를 표시합니다.
         return Integer.toString(gauge);
     }
+
+    //TODO: 필요한 경우 현재 게이지/최대 게이지 비율에 따른 iconFadePercent()를 구현해 주세요. 이건 모바일 화면에서의 버프 아이콘이 작기 때문에 현재 게이지 상태를 간접적으로 표현해 주는 역할을 합니다.
 
     private static final String GAUGE = "gauge";
     private static final String ELEMENT = "element";
 
     @Override
-    public void storeInBundle(Bundle bundle) {
+    public void storeInBundle(Bundle bundle) { // 변수들을 저장하는 메서드입니다.
         super.storeInBundle(bundle);
 
         bundle.put(GAUGE, gauge);
@@ -199,7 +216,7 @@ public class NewOverHeat extends Buff implements ActionIndicator.Action {
     }
 
     @Override
-    public void restoreFromBundle(Bundle bundle) {
+    public void restoreFromBundle(Bundle bundle) { // 세이브 로드 시 변수들을 불러오는 메서드입니다. 버튼도 다시 생성해 줍니다.
         super.restoreFromBundle(bundle);
 
         gauge = bundle.getInt(GAUGE);
@@ -211,12 +228,12 @@ public class NewOverHeat extends Buff implements ActionIndicator.Action {
     // 액션 인디케이터 관련 구현
 
     @Override
-    public String actionName() {
+    public String actionName() { // 버튼의 이름입니다.
         return Messages.get(this, "action_name");
     }
 
     @Override
-    public int actionIcon() {
+    public int actionIcon() { // 버튼의 아이콘입니다. 현재 원소에 따라 다른 아이콘을 적용합니다.
         switch (element) {
             case FIRE: default:
                 return HeroIcon.OVERHEAT_FIRE;
@@ -230,7 +247,7 @@ public class NewOverHeat extends Buff implements ActionIndicator.Action {
     }
 
     @Override
-    public Visual secondaryVisual() {
+    public Visual secondaryVisual() { // 버프 아이콘과 함께 현재 게이지/최대 게이지를 문자열로 표시해 줍니다.
         BitmapText txt = new BitmapText(PixelScene.pixelFont);
         txt.text(Messages.format(TXT_STATUS, gauge, OVERHEAT_MAX));
         switch (heatLevel()) {
@@ -254,13 +271,13 @@ public class NewOverHeat extends Buff implements ActionIndicator.Action {
                 break;
         }
 
-        txt.scale = new PointF(0.75f, 0.75f);
+        txt.scale = new PointF(0.75f, 0.75f); // 문자열 비주얼의 가로, 세로 스케일을 조정합니다.
         txt.measure();
         return txt;
     }
 
     @Override
-    public int indicatorColor() {
+    public int indicatorColor() { // 버튼을 물들이는 배경색입니다.
         switch (element) {
             case FIRE: default:
                 return 0xE24F2E;
@@ -274,13 +291,13 @@ public class NewOverHeat extends Buff implements ActionIndicator.Action {
     }
 
     @Override
-    public void doAction() {
+    public void doAction() { // 버튼을 누를 시 작동하는 메서드입니다. 여기에서는 원소 변경 선택 화면을 엽니다.
         GameScene.show(new WndElementSelect());
     }
 
-    public static class WndElementSelect extends WndOptions {
+    public static class WndElementSelect extends WndOptions { // 원소 변경 선택 클래스입니다.
 
-        public WndElementSelect() {
+        public WndElementSelect() { // 생성자입니다. 아이콘, 제목, 설명, 버튼, 버튼, 버튼... 순서로 구성되어 있습니다.
             super(Icons.get(Icons.ELEMENT),
                     Messages.titleCase(Messages.get(WndElementSelect.class, "title")),
                     Messages.get(WndElementSelect.class, "desc"),
@@ -293,10 +310,10 @@ public class NewOverHeat extends Buff implements ActionIndicator.Action {
 
         @Override
         protected void onSelect(int index) {
-            if (index < 4) {
+            if (index < 4) { // 취소 버튼 이외의 버튼을 누를 경우 이하 코드가 작동합니다.
                 if (Dungeon.hero == null) return;
 
-                switch (index) {
+                switch (index) { // 누른 버튼의 인덱스에 따라 다른 동작을 수행합니다.
                     case 0: default:
                         NewOverHeat.changeElement(Dungeon.hero, ElementType.FIRE);
                         break;
@@ -311,37 +328,37 @@ public class NewOverHeat extends Buff implements ActionIndicator.Action {
                         break;
                 }
 
-                //이펙트, 사운드 등 추가
+                NewOverHeat.onChangeElement();
 
-                BuffIndicator.refresh(Dungeon.hero);
-                ActionIndicator.refresh();
+                BuffIndicator.refresh(Dungeon.hero); // 버프창을 새로고침합니다.
+                ActionIndicator.refresh(); // 액션 버튼을 새로고침합니다.
 
-                hide();
+                hide(); // 윈도우를 숨깁니다.
             } else {
-                hide();
+                hide(); // 취소 버튼 선택 시 다른 동작을 하지 않고 윈도우를 숨깁니다.
             }
         }
 
         @Override
-        protected boolean enabled(int index) {
-            if (index == 4) return true; //취소 버튼은 항상 활성화
-            if (Dungeon.hero == null) return true;
-            NewOverHeat buff = Dungeon.hero.buff(NewOverHeat.class);
-            if (buff == null) return true;
-            return buff.getElement() != ElementType.getElementByIndex(index);
+        protected boolean enabled(int index) { // 버튼의 인덱스에 따른 활성화 여부입니다.
+            if (index == 4) return true; // 취소 버튼은 항상 활성화합니다.
+            if (Dungeon.hero == null) return true; // 윈도우 특성 상 영웅은 항상 있으나, 그래도 체크해 줍니다. 영웅이 없을 경우 버튼을 비활성화하지 않습니다.
+            NewOverHeat buff = Dungeon.hero.buff(NewOverHeat.class); // 영웅이 가진 버프의 인스턴스를 가져옵니다.
+            if (buff == null) return true; // 윈도우 특성 상 오버히트 버프 또한 항상 가지고 있을 것이나, 그래도 체크해 줍니다. 버프를 가지지 않을 경우 버튼을 비활성화하지 않습니다.
+            return buff.getElement() != ElementType.getElementByIndex(index); // 버프의 원소가 선택한 원소와 같지 않을 경우 버튼을 활성화합니다. 다시 말해, 현재 이미 지정되어 있는 원소로 바꾸는 버튼을 비활성화합니다.
         }
 
         @Override
-        protected boolean hasInfo(int index) {
+        protected boolean hasInfo(int index) { // 취소 버튼을 제외한 모든 버튼에 인포 버튼을 달아 줍니다.
             return index < 4;
         }
 
         @Override
-        protected void onInfo( int index ) {
+        protected void onInfo( int index ) { // 인포 버튼 클릭 시 인덱스에 따라 작동하는 메서드입니다.
             GameScene.show(new WndTitledMessage(
-                    Icons.get(Icons.INFO),
-                    Messages.titleCase(Messages.get(WndElementSelect.class, "info_title", ElementType.getElementByIndex(index).getName())),
-                    Messages.get(WndElementSelect.class, "info_"+ElementType.getElementByIndex(index).name)));
+                    Icons.get(Icons.INFO), // 설명 윈도우의 아이콘입니다.
+                    Messages.titleCase(Messages.get(WndElementSelect.class, "info_title", ElementType.getElementByIndex(index).getName())), // 설명 윈도우의 제목입니다.
+                    Messages.get(WndElementSelect.class, "info_"+ElementType.getElementByIndex(index).name))); // 설명 윈도우의 각 원소에 따른 설명문입니다.
         }
     }
 }
